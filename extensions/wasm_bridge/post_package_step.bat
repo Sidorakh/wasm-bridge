449276d5d768b91e9df106ba8f635fdb72369889
Andrew Rutherford <2681131+Sidorakh@users.noreply.github.com>
2024-09-24 20:55:56 +1000

    Attempted to make it work on export - may work with zip builds, does not work no GX Games

---
+++ extensions/wasm_bridge/post_package_step.bat
@@ -0,0 +1,14 @@
+REM @echo off
+if "%YYEXTOPT_wasm_bridge_enabled%" == "True" (
+    echo "%YYEXTOPT_wasm_bridge_enabled%"
+    if exist "%YYMACROS_project_dir%/extensions/wasm_bridge/scripts/server.js"  (
+        set loc=%cd%
+        cd "%YYMACROS_project_dir%/extensions/wasm_bridge/scripts/"
+        cmd /c "npm i"
+        node "%YYMACROS_project_dir%/extensions/wasm_bridge/scripts/server.js" postpackage
+    ) else (
+        echo script file not found
+    )
+) else (
+    echo extension wasm_bridge not enabled - continuing to regular build
+)
--- extensions/wasm_bridge/scripts/server.js
+++ extensions/wasm_bridge/scripts/server.js
@@ -1,10 +1,16 @@
 const cheerio = require('cheerio');
 const path = require('path');
 const fs = require('fs');
-
+const JSZip = require('jszip'); // for modifying the output zip file
 // assume it's enabled - this script *is* running after all
 
-const runner_path = path.join(process.env.YYoutputFolder,'runner','runner.html');
+fs.writeFileSync(path.join(__dirname,'../','../','../','.env'),Object.entries(process.env).map(e=>`${e[0]}=${e[1]}`).join('\n'))
+
+
+let runner_path = path.join(process.env.YYoutputFolder,'runner','runner.html');
+if (!fs.existsSync(runner_path)) {
+    runner_path = path.join(process.env.YYoutputFolder,'runner','index.html');
+}
 const runner_html = fs.readFileSync(runner_path,'utf8');
 
 const inject_js = fs.readFileSync(path.join(__dirname,'ext-setup.js'),'utf8');
@@ -22,6 +28,38 @@ for (const file of files) {
     }
 }
 
-$('head').prepend(`<script>\n${inject_js}\n</script>`);
+$('head').prepend(`\n<script>\n${inject_js}\n</script>`);
 
 fs.writeFileSync(runner_path,$.html());
+let is_post_package = false;
+for (const arg of process.argv.slice(2)) {
+    if (arg.includes('postpackage')) {
+        console.log('we are IN THE POST PACKAGE STEP BOIIIIIII WOOOO!!');
+        console.log(`Target File: ${process.env.YYtargetFile}`);
+        is_post_package = true;
+        
+        fs.copyFileSync(process.env.YYtargetFile, path.join(__dirname,'game.zip'));
+    }
+
+}
+
+if (is_post_package) {
+    (async()=>{
+        const data = fs.readFileSync(process.env.YYtargetFile);
+        const zip = await JSZip.loadAsync(data);
+        for (const file of files) {
+            if (file.endsWith('.js')) {
+                const js = fs.readFileSync(path.join(__dirname,'libraries',file));
+                zip.file(`libraries/${file}`,js);
+            }
+        }
+        zip.file('index.html',$.html());
+        const final = await zip.generateAsync({type: 'nodebuffer'});
+        fs.writeFileSync(process.env.YYtargetFile,final);
+
+        console.log('Overwritten final zip file - pausing');
+        //fs.copyFileSync(process.env.YYtargetFile, path.join(__dirname,path.basename(process.env.YYtargetFile)));
+        process.exit(0);
+    })();
+
+}
